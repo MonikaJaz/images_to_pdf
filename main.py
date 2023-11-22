77629ee4283f30d646a085125fabf5ebe66878cf
MonikaJaz <145592148+MonikaJaz@users.noreply.github.com>
2023-11-22 22:55:45 +0100

    commit

--- main.py
+++ main.py
@@ -10,24 +10,34 @@ def merge_images_to_pdf(input_folder, output_pdf):
         return
 
     # sorting files by order
-    image_files.sort()
+    image_files.sort(key=lambda x: int(x.split('.')[0]))
 
     pdf = FPDF()
 
     for image_file in image_files:
         image_path = os.path.join(input_folder, image_file)
         image = Image.open(image_path)
+        if image.width > pdf.w or image.height > pdf.h:
+            ratio = min(pdf.w / image.width, pdf.h / image.height)
+            width = image.width * ratio
+            height = image.height * ratio
+        else:
+            width = image.width
+            height = image.height
+
         pdf.add_page()
-        pdf.image(image_path, 0, 0, pdf.w, pdf.h)
+        pdf.image(image_path, x=pdf.w / 2 - width / 2, y=pdf.h / 2 - height / 2, w=width, h=height)
+
+        pdf.add_page()
+        pdf.image(image_path, 10, 10, pdf.w - 20, pdf.h)
 
 
-   # script_directory = os.path.dirname(os.path.abspath(__file__))
     output_pdf_path = os.path.join(input_folder, output_pdf)
 
     pdf.output(output_pdf_path, "F")
 
     print(f"The images have been merged to the pdf file: {output_pdf_path}")
 
-input_folder = "C:/Users/ADMIN/Desktop/zdjecia2"
-output_pdf = "merged.pdf"
+input_folder = "folder path"
+output_pdf = "name.pdf"
 merge_images_to_pdf(input_folder, output_pdf)
