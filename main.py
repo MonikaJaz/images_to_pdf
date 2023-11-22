496aa1c1fb04d657c5f98efd5c982095d5e9f99b
MonikaJaz <145592148+MonikaJaz@users.noreply.github.com>
2023-11-22 21:36:00 +0100

    commit

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ from PIL import Image
 from fpdf import FPDF
 import os
 
-
 def merge_images_to_pdf(input_folder, output_pdf):
     image_files = [f for f in os.listdir(input_folder) if f.endswith(('.png', '.jpg', '.jpeg'))]
-
 
     if not image_files:
         print("There is no photos to convert to pdf!.")
@@ -23,12 +21,13 @@ def merge_images_to_pdf(input_folder, output_pdf):
         pdf.image(image_path, 0, 0, pdf.w, pdf.h)
 
 
-    pdf.output(output_pdf, "File")
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    output_pdf_path = os.path.join(script_directory, output_pdf)
 
-    print(f"The images have been merged to the pdf file: {output_pdf}")
+    pdf.output(output_pdf_path, "F")
 
+    print(f"The images have been merged to the pdf file: {output_pdf_path}")
 
-
-input_folder = "file path"
-output_pdf = "name of the pdf file.pdf"
+input_folder = "C:/Users/ADMIN/Desktop/zdjecia2"
+output_pdf = "merged.pdf"
 merge_images_to_pdf(input_folder, output_pdf)
